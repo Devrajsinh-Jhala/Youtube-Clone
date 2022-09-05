9b3a307eb0c804aa02bf9c464ba35252af37d906
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2022-09-05 10:56:41 +0530

    Added new logos

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,6 +11,7 @@ import GraphicEqIcon from "@mui/icons-material/GraphicEq";
 import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+import JavascriptIcon from "@mui/icons-material/Javascript";
 
 export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
 
@@ -18,7 +19,7 @@ export const categories = [
   { name: "New", icon: <HomeIcon /> },
   { name: "JS Mastery", icon: <CodeIcon /> },
   { name: "Coding", icon: <CodeIcon /> },
-  { name: "ReactJS", icon: <CodeIcon /> },
+  { name: "ReactJS", icon: <JavascriptIcon /> },
   { name: "NextJS", icon: <CodeIcon /> },
   { name: "Music", icon: <MusicNoteIcon /> },
   { name: "Education", icon: <SchoolIcon /> },
